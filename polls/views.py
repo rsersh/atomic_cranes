29b87547208b9431b35317977e22fd24ebd07740
Rachel Sershon <rsersh@gmail.com>
2015-06-17 17:54:41 -0700

    more template work

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 #from django.http import Http404
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+#from django.http import HttpResponse
+#from django.template import RequestContext, loader
 
 from .models import Question
 
