5b4d72c287a5666aad15ceeea9b49e9c98d15abf
Rachel Sershon <rsersh@gmail.com>
2015-07-02 17:54:10 -0700

    added tests for results view

--- polls/tests.py
+++ polls/tests.py
@@ -43,6 +43,26 @@ def create_question(question_text, days):
   time = timezone.now() + datetime.timedelta(days=days)
   return Question.objects.create(question_text=question_text, pub_date=time)
 
+class QuestionIndexResultsTests(TestCase):
+  def test_results_view_with_a_future_question(self):
+    """
+    The detail view of a question with a pub_date in the future should
+    return a 404 not found..
+    """
+    future_question = create_question(question_text="Future question.", days=5)
+    response = self.client.get(reverse('polls:results', args=(future_question.id,)))
+    self.assertEqual(response.status_code, 404) 
+  
+  def test_results_view_a_past_question(self):
+    """
+    The results view of a question with a pub_date in the past should
+    display the question.
+    """ 
+    past_question = create_question(question_text='Past Question.',
+                           days = -5)
+    response = self.client.get(reverse('polls:results', args=(past_question.id,)))
+    self.assertContains(response, past_question.question_text, status_code=200)
+
 class QuestionViewTests(TestCase):
   def test_index_view_with_no_questions(self):
     """
