b3d73574af95dbe1ba160de3c543cc19c641f848
Rachel Sershon <rsersh@gmail.com>
2015-07-02 19:43:03 -0700

    added code to only pull questions with choices that have been published

--- polls/views.py
+++ polls/views.py
@@ -23,10 +23,16 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
    model = Question
    template_name = 'polls/detail.html'
-
+   question_choice_set = []
    def get_queryset(self):
      """
-     Excludes any questions that aren't published yet.
+     Excludes any questions that aren't published yet or do not have choices.
+     choice_set = Choice.objects.all()
+     for c in choice_set:
+       if (c.question.pub_date <= timezone.now()):
+          question_choice_set.append(c.question)
+     return set(question_choice_set)
+
      """
      return Question.objects.filter(pub_date__lte = timezone.now())
 
@@ -39,6 +45,14 @@ class ResultsView(generic.DetailView):
      Excludes any questions that aren't published yet.
      """
      return Question.objects.filter(pub_date__lte = timezone.now())
+
+def get_questions_with_choices(self):
+   q_list = []
+   choice_set = Choice_objects_all()
+   for c in choice_set:
+      if (c.question.pub_date <= timezone.now()):
+         q_list.append(c.question)
+   return set(q_list)
 
 def vote(request, question_id):
    p= get_object_or_404(Question, pk=question_id)
